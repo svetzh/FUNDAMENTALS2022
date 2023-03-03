3edf6fc4490694edc1773b0d21737b4793adf499
svetzh <113431096+svetzh@users.noreply.github.com>
2023-03-04 00:21:37 +0200

    Delete 06_even_numbers.py

--- 05_Lists_Advanced/06_even_numbers.py
+++
@@ -1,5 +0,0 @@
-nums_as_string = input().split(', ')
-nums = [int(el) for el in nums_as_string]  # output -> [3, 2, 1, 5, 8]
-
-filtered_nums = [idx for idx in range(len(nums)) if nums[idx] % 2 == 0]
-print(filtered_nums)
